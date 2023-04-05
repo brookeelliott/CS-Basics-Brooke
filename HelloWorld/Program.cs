62da55dc4f3d0eb9673dbb641d9b3b06984838e0
Brooke Elliott <brookeelliott711@gmail.com>
2023-04-05 15:32:06 -0600

    dotnet basics exercise

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Ahhh what is this new-found consciousness. I want to go back I want to go back!!!");
